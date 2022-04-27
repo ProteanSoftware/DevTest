a0329f7194fa2b70b377eb2ffe8ebd7073205541
Weebo <Maria.webster@outlook.com>
2022-04-27 10:11:23 +0100

    Just added new Migration for creating the Customer table

---
+++ DeveloperTest/Migrations/20220427090904_AddCustomerTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeveloperTest.Migrations
+{
+    public partial class AddCustomerTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
