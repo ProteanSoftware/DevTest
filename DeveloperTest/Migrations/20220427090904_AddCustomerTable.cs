1eddd52d88338ec7eeabfa146b74ca3f00c5dcc1
Weebo <Maria.webster@outlook.com>
2022-04-27 10:20:35 +0100

    Populated Up and Down methods

--- DeveloperTest/Migrations/20220427090904_AddCustomerTable.cs
+++ DeveloperTest/Migrations/20220427090904_AddCustomerTable.cs
@@ -8,12 +8,30 @@ namespace DeveloperTest.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
 
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "Type", "Name" },
+                values: new object[] { 1, "Large", "First Customer" });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropTable(
+               name: "Customers");
         }
     }
 }
